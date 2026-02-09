3347df7bbb4db4baa3183c26c1b6a90858d11a02
Lalith Kumar Goud Nanam <nanamlalithkumar@gmail.com>
2026-02-09 13:49:30 +0530

    c11

--- frontend/src/js/api.js
+++ frontend/src/js/api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://YOUR-BACKEND-URL.onrender.com";
+    : "https://novus-7h23.onrender.com";
 
 export default BASE_URL;
