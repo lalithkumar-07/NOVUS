c65ca4236af0ce42719174d84a75c9a3baa8b216
Lalith Kumar Goud Nanam <nanamlalithkumar@gmail.com>
2026-02-11 14:43:50 +0530

    c25

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ export function loadFooter() {
   footer.innerHTML = `
 <footer class="border-t border-white/10 py-10 text-center mt-20">
   <p class="text-gray-400">
-    © 2026 NOVUS Community • Hackathon Portal
+    © 2026 THE NOVUS • Hackathon Portal
   </p>
 </footer>
 `;
